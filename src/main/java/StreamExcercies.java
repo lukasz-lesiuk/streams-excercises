28b609f2cb31374d8cfc1169720e583907d918aa
praczkowski <p.raczkowski@ocado.com>
2020-07-04 09:41:20 +0200

    Initial commit

---
+++ src/main/java/StreamExcercies.java
@@ -0,0 +1,29 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamExcercies {
+
+    public int addUp(List<Integer> numbers) {
+        return 0;
+    }
+
+    public List<String> namesAndOrigin(List<Artist> artists) {
+        return Collections.emptyList();
+    }
+
+    public List<Album> getAlbumsWithAtMoseThreeTracks(List<Album> albums) {
+        return Collections.emptyList();
+    }
+
+    public int calculateMembers(List<Artist> artists) {
+        int totalMembers = 0;
+        for (Artist artist : artists) {
+            Stream<Artist> members = artist.getMembers();
+            totalMembers += members.count();
+        }
+
+        return totalMembers;
+    }
+
+}
